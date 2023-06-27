19dba5457a52abdc7d08422fb9e6d6d9f4a1b965
brunovalenzisi <130124652+brunovalenzisi@users.noreply.github.com>
2023-06-27 12:42:09 -0300

    version grafica en construccion

--- js/clases.js
+++ js/clases.js
@@ -23,15 +23,16 @@ class Maso {
     sacarCarta(){
 
             //genera un numero aleatorio comprendido en el total de cartas del maso actual
-            let min=1
-            let max=this.cartas.length
+            if(this.cartas.length>0){
+            let min=0
+            let max=this.cartas.length-1
             let cartaRandom=Math.floor((Math.random() * (max - min + 1)) + min);
             let carta=this.cartas[cartaRandom]
             this.cartas.splice(cartaRandom,1)
             return carta
-
-
            }
+            }
+            
     }
 
 
@@ -70,38 +71,137 @@ class Carta{
         
         girar(){
             if(this.cara==false){
-            this.cara=true}
+            this.cara=true
+            }
             else if(this.cara==true){this.cara=false
             }
         }
         }
     
     class Mesa{
-    jugador1=[]
-    jugador2=[]
+    manos=[]
     constructor(){
-        this.jugador1=[]
-        this.jugador2=[]
+    this.manos=[new Mano(1),new Mano(2),new Mano(3),new Mano(4)]
+    
     }
-        crearCarta(carta,jugador){
-                    let divMesa = document.getElementById('displayMesa')
-                    let nuevoDiv=document.createElement("div")
-                    nuevoDiv.style.backgroundImage=`url(${carta.srcImagen})`
-                    if(jugador==1){
-                    nuevoDiv.style.left=`${36+this.jugador1.length*5}%`  
-                    this.jugador1.push(carta)
-                    nuevoDiv.classList.add("carta1")}
-                    else if(jugador==2){
-                    nuevoDiv.style.left=`${36+this.jugador2.length*5}%`  
-                    this.jugador2.push(carta)
-                    nuevoDiv.classList.add("carta2")
-                    }
-                    nuevoDiv.classList.add("animate__animated")
-                    nuevoDiv.classList.add("animate__backInLeft")
-                    
-                    divMesa.appendChild(nuevoDiv)                   
+        entregarCarta(mano,baraja){
+            if(baraja.cartas.length>0){
+            let nCartas
+            let div
+            let nuevaCarta=baraja.sacarCarta()
+            
+            if(mano==1){
+                div= document.getElementById("mano1")
+                this.manos[0].cartas.push(nuevaCarta)
+                this.manos[0].abrir()
                 
             }
+                else if(mano==2){
+                    div= document.getElementById("mano2")
+                    this.manos[1].cartas.push(nuevaCarta)
+                    this.manos[1].abrir()
+                    
+                }
+            else if(mano==3){
+                div= document.getElementById("mano3")
+                this.manos[2].cartas.push(nuevaCarta)
+                this.manos[2].abrir()
+                
+               }
+            else if(mano==4){
+                div= document.getElementById("mano4")
+                this.manos[3].cartas.push(nuevaCarta)
+                
+               }
+
+                    let nuevoDiv=document.createElement("div")
+                    nuevoDiv.style.backgroundImage=`url(${nuevaCarta.srcImagen})`
+                    
+                    if(mano==1){
+                       nCartas=this.manos[0].cartas.length
+                       nuevoDiv.style.left=`${(nCartas-1)*30}%`
+                       nuevoDiv.classList.add("animate__animated")    // ToDo:convertir a la funcion animarElemento
+                       nuevoDiv.classList.add("animate__backInLeft")
+                       
+                    }
+                        else if(mano==2){
+                        nCartas=this.manos[1].cartas.length
+                        nuevoDiv.style.left=`${(nCartas-1)*30}%`
+                        nuevoDiv.classList.add("animate__animated")
+                        nuevoDiv.classList.add("animate__backInDown")
+                       
+                    }
+                    else if(mano==3){
+                        nCartas=this.manos[2].cartas.length
+                        nuevoDiv.style.left=`${(nCartas-1)*30}%`
+                        nuevoDiv.classList.add("animate__animated")
+                        nuevoDiv.classList.add("animate__backInRight")
+                        
+                    }
+                    else if(mano==4){
+                        nCartas=this.manos[3].cartas.length
+                        nuevoDiv.style.left=`${(nCartas-1)*30}%`
+                        nuevoDiv.classList.add("animate__animated")
+                        nuevoDiv.classList.add("animate__backInDown")
+                        
+                    }
+
+                        nuevoDiv.classList.add("carta")
+                        div.appendChild(nuevoDiv)
+                        
+                    
+
+            }   
+                            }
         }
+
+        class Mano{
+            cartas=[]
+            peso
+            id
+            cerrada
+            constructor(id){
+                this.cartas=[]
+                this.peso=0
+                this.id=id
+                this.cerrada=true
+                
+            }
+        
+
+            contarCartas(){  //funcion auxiliar de evaluar, suma los puntos de una mano deseada
+                let cantidadDeAs=0
+                let acumulado=0
+                for(let i=0;i<this.cartas.length;i++){
+                if(this.cartas[i].valor[0]==1){
+                cantidadDeAs++;
+                continue   // si la carta actual es uno, suma una unidad al contador de ases y continua el ciclo para evaluarlos al final
+                }else{acumulado+=this.cartas[i].valor[0];}    
+                 }
+               
+            if(cantidadDeAs>0){   //elvalua los 1 y les da el valor adecuado a la mano actual
+                for(let i =0;i<cantidadDeAs;i++){
+                    if(acumulado+11+cantidadDeAs-1<=21){
+                        acumulado+=11
+                    }else{acumulado++}
+                }
+                
+            }
+            this.peso=acumulado
+                }
+                cerrar(){
+                    this.cerrada=true
+                }
+                abrir(){
+                    this.cerrada=false
+                }
+            
+                
+               
+            }
+            
+        
+
+        
     
     
--- js/script.js
+++ js/script.js
@@ -1,216 +1,198 @@
-window.addEventListener("load",main,false)
-let maso1;
+window.addEventListener("load",pantallaInicial,false)
+let maso;
 let mesa;
-function main(){
-
-let manoJugador1=[];
-let manoJugador2=[];
-let pesoJugador1=0;
-let pesoJugador2=0;
-let jugador1="";
-let jugador2 = "crupier"
-let partidaEnProgreso=false //define el estado actual de la partida
-let primeraMano=true
-maso1=new Maso(false);
-mesa=new Mesa
+let nodeMesa=document.getElementById("mesa")
+let nodeMano1=document.getElementById("mano1")
+let nodeMano2=document.getElementById("mano2")
+let nodeMano3=document.getElementById("mano3")
+let nodeMano4=document.getElementById("mano4")
+let node10=document.getElementById("apostar10")
+let node100=document.getElementById("apostar100")
+let node500=document.getElementById("apostar500")
+let node1000=document.getElementById("apostar1000")
 
 
 
-function iniciarInterface(){
-console.clear();
-let nuevaPartida;
-if(primeraMano){
-nuevaPartida=prompt("Desea empezar una nueva partida de consoleBlackJack?(Y/N)");
-primeraMano=false
+
+
+
+
+function pantallaInicial(){
+    let botonIniciar=document.createElement("h1")
+    botonIniciar.classList.add("botonIniciar")
+    botonIniciar.id="botonIniciar"
+    botonIniciar.innerHTML="Comenzar Partida"
+    botonIniciar.addEventListener("click",partidaConsoleBlackJack,false)
+    nodeMesa.appendChild(botonIniciar)
+    animarClase("botonIniciar","bounce")
 }
 
-else{nuevaPartida=prompt("Desea empezar una nueva mano de consoleBlackJack?(Y/N)");}
-    
-if(nuevaPartida.toLowerCase()=="y"){  // se utiliza el metodo toLowerCase() para salvar las mayusculas
-partidaEnProgreso=true;                           
-if(jugador1==""){jugador1=prompt("Ingresa tu nombre");}  // se utiliza el if para no volver a preguntar el nombre en siguientes rondas
-console.log("❤️♠️🔶🍀 Que empiece el juego "+ jugador1 + "! ❤️♠️🔶🍀")    
-partidaConsoleBlackJack(); 
-}else if(nuevaPartida.toLowerCase()=="n" ){
-partidaEnProgreso=false
-console.log("❤️♠️🔶🍀 Gracias! vuelva prontosss ❤️♠️🔶🍀")
-jugador1=""
-}else{
-alert("💀💀 ah ah aaahhh..no dijiste las palabras magicas..💀💀")
-iniciarInterface();
-}
-}
 
-function partidaConsoleBlackJack(){          //es la funcion principal del programa
-resetearPartida(); 
-empezarJuego(); 
-if(partidaEnProgreso==true){siguienteRonada();} //se utiliza el if para evitar caminos indeseados
-}
 
-function empezarJuego(){    //reparte las cartas y evalua la primera mano
-
-manoJugador1.push(maso1.sacarCarta(),maso1.sacarCarta())
-manoJugador2.push(maso1.sacarCarta(),maso1.sacarCarta())
-evaluar();
-mostrarMano(jugador1,manoJugador1,pesoJugador1);
-resultado(jugador1)
-}
-
-function pedirCarta(mano){         //ingresa una nueva carta random en la mano del jugador que se desee
-    mano.push(maso1.sacarCarta());
-       
-    }
-
-function evaluar(){            //actualiza las variables
-    pesoJugador1=contarCartas(manoJugador1);
-    pesoJugador2=contarCartas(manoJugador2);
-         
-
-   function contarCartas(mano){  //funcion auxiliar de evaluar, suma los puntos de una mano deseada
-    let cantidadDeAs=0
-    let acumulado=0
-    for(let i=0;i<mano.length;i++){
-    if(mano[i].valor[0]==1){
-    cantidadDeAs++;
-    continue   // si la carta actual es uno, suma una unidad al contador de ases y continua el ciclo para evaluarlos al final
-    }else{acumulado+=mano[i].valor[0];}    
-     }
-   
-if(cantidadDeAs>0){   //elvalua los 1 y les da el valor adecuado a la mano actual
-    for(let i =0;i<cantidadDeAs;i++){
-        if(acumulado+11+cantidadDeAs-1<=21){
-            acumulado+=11
-        }else{acumulado++}
+function pedir(mano){
+    if(mesa.manos[mano-1].peso<21){
+    mesa.entregarCarta(mano,maso)
+    evaluar();
+    resultado(1,mesa.manos[mano-1]);
     }
     
-}
-return acumulado
     }
 
-    
-   
-}
 
-function resetearPartida(){   //inicializa las variables
-manoJugador1=[];
-manoJugador2=[];
-pesoJugador1=0;
-pesoJugador2=0;
-}
-
-function siguienteRonada(){     // es un ciclo que termina cuando uno decide plantarse invocando la funcion "plantarse()"
-   let seguir=prompt("Te plantas??🐔 (Y/N)")
-   if(seguir.toLowerCase()=="n"){
-   pedirCarta(manoJugador1);
-   evaluar();
-   mostrarMano(jugador1,manoJugador1,pesoJugador1);
-   resultado(jugador1);
-   if(partidaEnProgreso==true){siguienteRonada();}
-
-   }else if(seguir.toLowerCase()=="y"){
-   plantarse();
-   }else{
-    alert("💀💀 ah ah aaahhh..no dijiste las palabras magicas..💀💀");
-    siguienteRonada();
-   }
-}
-
-function mostrarMano(player,mano,peso){    //muestra la mano de un jugador deseado
-    console.log("la mano de " + player + " es: ")
-    for(let i=0;i<mano.length;i++){
-        console.log("Carta " + (i+1)+":"+mano[i].id)
-    }
-    console.log("Suman " + peso+ " puntos")
-}
+    const animarClase = (element, animation, prefix = 'animate__') =>
+  
+    new Promise((resolve, reject) => {
+      const animationName = `${prefix}${animation}`;
+      const node = document.getElementById(element);
+  
+      node.classList.add(`${prefix}animated`, animationName);
+  
+      
+      function handleAnimationEnd(event) {
+        event.stopPropagation();
+        node.classList.remove(`${prefix}animated`, animationName);
+        resolve('Animation ended');
+      }
+  
+      node.addEventListener('animationend', handleAnimationEnd, {once: true});
+    });
 
 
 
-function plantarse(){   // a partir de aqui, comienza a jugar el programa
-    
-    
-        mostrarMano(jugador2,manoJugador2,pesoJugador2);
-        while(pesoJugador2<17){  // el ciclo termina cuando se cumpla la condicion del reglamneto del juego
-            confirm("La mano del crupier es menor o igual a 17, pide otra carta");
-            pedirCarta(manoJugador2);
-            evaluar();
-            mostrarMano(jugador2,manoJugador2,pesoJugador2);
-            
-            }
-            resultado(jugador2) // el hecho de que salga del ciclo implica el fin definitivo de la mano actual
-        }
-         
-    
-    
-    
-
-
-
-function resultado(jugador){   // evalua la situacion de la partida en diferentes estados de la misma, se utiliza el parametro jugador para definir estos estados.
-    if (jugador==jugador1){if(manoJugador1.length==2 && pesoJugador1==21){
-        partidaEnProgreso=false 
-        console.log("❤️♠️🔶🍀 Felicitaciones! Conseguiste un BlackJack y ganaste la partida! ❤️♠️🔶🍀")
-        confirm("Continuar");     
-        iniciarInterface();
+ // evalua la situacion de la partida en diferentes estados de la misma, se utiliza el parametro jugador para definir estos estados.
+    function resultado(jugador,mano){ 
+        if(mano.id<4 && mano.peso>0){ 
+        if (jugador==1){if(mano.cartas.length==2 && mano.peso==21){
+            console.log("❤️♠️🔶🍀 Felicitaciones! Conseguiste un BlackJack y ganaste la partida! ❤️♠️🔶🍀")
+            confirm("Continuar");     
             }  else
-            if(pesoJugador1==21){
-                console.log("Conseguiste 21 puntos, te plantas");
-                confirm("Continuar");
-                plantarse();
-                }
-              else if(pesoJugador1>21){
-                partidaEnProgreso=false;
-                console.log("💀💀 Perdiste! sumaste mas de 21 puntos 💀💀")
-                confirm("Continuar");
-                iniciarInterface();}}
-                
-                else if(jugador==jugador2){
-                    if(pesoJugador2>21){
-                        partidaEnProgreso=false;
-                        console.log("El crupier se paso")
-                        console.log(" ❤️♠️🔶🍀 Felicitaciones! ganaste la partida! ❤️♠️🔶🍀")
-                        confirm("Continuar");
-                        iniciarInterface();}
-                        else if(pesoJugador1>pesoJugador2){
-                            partidaEnProgreso=false;
-                            console.log("❤️♠️🔶🍀 Felicitaciones! ganaste la partida! ❤️♠️🔶🍀")
-                            confirm("Continuar"); 
-                            iniciarInterface();
-                        }else if (pesoJugador1<pesoJugador2){
-                            partidaEnProgreso=false;
-                            console.log("💀💀 Perdiste! El crupier sumo mas puntos 💀💀")
-                            confirm("Continuar");
-                            iniciarInterface(); 
-                            
-                        }else if(pesoJugador1==pesoJugador2){
-                            partidaEnProgreso=false;
-                            console.log("😐La partida resulto en empate😐");
-                            confirm("Continuar");
-                            iniciarInterface();       
+                if(mano.peso==21){
+                    console.log("Conseguiste 21 puntos, te plantas");
+                    confirm("Continuar");
+                    plantarse();
                     }
+                  else if(mano.peso>21){
+                    console.log("💀💀 Perdiste! sumaste mas de 21 puntos 💀💀")
+                    confirm("Continuar");
+                    }}
+                    
+                    else if(jugador==2){
+                        if(mesa.manos[3].peso>21){
+                            console.log("El crupier se paso")
+                            console.log(" ❤️♠️🔶🍀 Felicitaciones! ganaste la partida! ❤️♠️🔶🍀")
+                            confirm("Continuar");
+                            }
+                            else if(mano.peso>mesa.manos[3].peso){
+                                console.log("❤️♠️🔶🍀 Felicitaciones! ganaste la partida! ❤️♠️🔶🍀")
+                                confirm("Continuar"); 
+                                }else if (mano.peso<mesa.manos[3].peso){
+                                console.log("💀💀 Perdiste! El crupier sumo mas puntos 💀💀")
+                                confirm("Continuar");
+                                
+                                
+                            }else if(mano.peso==mesa.manos[3].peso){
+                                console.log("😐La partida resulto en empate😐");
+                                confirm("Continuar");
+                                }
+                    }
+                            
+    
+                    }
+    }
 
+
+    function plantarse(){   // a partir de aqui, comienza a jugar el programa
+    
+    while(mesa.manos[3].peso<17){  // el ciclo termina cuando se cumpla la condicion del reglamneto del juego
+            confirm("La mano del crupier es menor o igual a 17, pide otra carta");
+            mesa.entregarCarta(4,maso);
+            evaluar();
+            }
+            for(let i=0;i<3;i++){
+                resultado(2,mesa.manos[i])
+            } // el hecho de que salga del ciclo implica el fin definitivo de la mano actual
+        }
+   
+   
+        function siguienteRonada(){     // es un ciclo que termina cuando uno decide plantarse invocando la funcion "plantarse()"
+            let seguir=prompt("Te plantas??🐔 (Y/N)")
+            if(seguir.toLowerCase()=="n"){
+            pedirCarta(manoJugador1);
+            evaluar();
+            mostrarMano(jugador1,manoJugador1,pesoJugador1);
+            resultado(jugador1);
+            if(partidaEnProgreso==true){siguienteRonada();}
+         
+            }else if(seguir.toLowerCase()=="y"){
+            plantarse();
+            }else{
+             alert("💀💀 ah ah aaahhh..no dijiste las palabras magicas..💀💀");
+             siguienteRonada();
+            }
+         }
+
+         //actualiza las variables
+    function evaluar(){ 
+    mesa.manos.forEach(mano =>mano.contarCartas());           
+     }
+
+     function resetearPartida(){       //inicializa las variables
+        mesa = new Mesa
+        maso=new Maso
+        }
+        
+
+
+        function partidaConsoleBlackJack(){          //es la funcion principal del programa
+            nodeMesa.removeChild(nodeMesa.lastChild)
+            resetearPartida();
+            empezarJuego();
+            
+            /*if(partidaEnProgreso==true){siguienteRonada();} //se utiliza el if para evitar caminos indeseados
+            */}
+
+
+            function empezarJuego(){    //reparte las cartas y evalua la primera mano
+
+                mesa.entregarCarta(1,maso)
+                mesa.entregarCarta(1,maso)
+                mesa.entregarCarta(4,maso)
+                mesa.entregarCarta(4,maso)
+                evaluar();
+                mesa.manos.forEach(mano => resultado(1,mano));
                 }
-}
-function reglamento(){
-    console.log("🃏Bienvenido a ConsoleBlackJack!")
-    console.log("🃏El objetivo el sumar 21 o el valor mas cercano posible")
-    console.log("🃏Las reglas son sencillas:")
-    console.log("🃏Al empezar se te otorgan 2 cartas.")
-    console.log("🃏Las cartas van del 1 al 10 y no tienen palo")
-    console.log("🃏El 1 vale tanto 1 como 11")
-    console.log("🃏Si conseguis sumar 21 con la mano inicial, es la mejor jugada posible, ganas la ronda automaticamente")
-    console.log("🃏Podras pedir mas cartas para sumar a tu mano")
-    console.log("🃏Pero, cuidado!, si te pasas de 21 perdes la partida")
-    console.log("🃏Cuando decidas plantarte, el crupier muestra sus cartas")
-    console.log("🃏Si tiene menos de 17 esta obligado a pedir otra carta")
-    console.log("🃏Si tiene 17 o mas esta obligado a plantarse")
-    console.log("🃏Si el crupier se pasa de 21 puntos, ganas la partida")
-    console.log("🃏Una vez que los dos se plantan, se suman los puntos")
-    console.log("🃏El jugador con mas puntos gana la ronda")
-    alert("Cuando estes listo pulsa Aceptar")
-    iniciarInterface()
 
-}
-}
+
+
+         function reglamento(){
+            console.log("🃏Bienvenido a ConsoleBlackJack!")
+            console.log("🃏El objetivo el sumar 21 o el valor mas cercano posible")
+            console.log("🃏Las reglas son sencillas:")
+            console.log("🃏Al empezar se te otorgan 2 cartas.")
+            console.log("🃏Las cartas van del 1 al 10 y no tienen palo")
+            console.log("🃏El 1 vale tanto 1 como 11")
+            console.log("🃏Si conseguis sumar 21 con la mano inicial, es la mejor jugada posible, ganas la ronda automaticamente")
+            console.log("🃏Podras pedir mas cartas para sumar a tu mano")
+            console.log("🃏Pero, cuidado!, si te pasas de 21 perdes la partida")
+            console.log("🃏Cuando decidas plantarte, el crupier muestra sus cartas")
+            console.log("🃏Si tiene menos de 17 esta obligado a pedir otra carta")
+            console.log("🃏Si tiene 17 o mas esta obligado a plantarse")
+            console.log("🃏Si el crupier se pasa de 21 puntos, ganas la partida")
+            console.log("🃏Una vez que los dos se plantan, se suman los puntos")
+            console.log("🃏El jugador con mas puntos gana la ronda")
+            alert("Cuando estes listo pulsa Aceptar")
+            iniciarInterface()
+        
+        }
+
+
+
+
+
+
+
+ 
+
 
 
 
